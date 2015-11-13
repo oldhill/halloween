cfc9ae7ca5c367c540044af83974c0ac4fa6d44f
oldhill <oldhillcode@gmail.com>
2015-11-13 09:35:13 -0800

    Find all matches

--- regex.py
+++ regex.py
@@ -37,3 +37,30 @@ print 'parsing it out:'
 search_results = pattern.search('&x@y.com*---')
 print dir(search_results)
 print search_results.group(0)
+
+
+# Finding all matches
+print ''
+pattern = re.compile('[a-z]+@[a-z]+\.com')
+search_results = pattern.findall('---foo@bar.com---()*jdjhgjdh&&66x@y.com***')
+print search_results
+
+# Multi-line strings
+print ''
+my_string = """ Dear X,
+
+                Hi! Have you tried coding in Python? It's
+                pretty cool. Very fun to work with and expressive,
+                although the dynamic type system can wreak havoc in
+                large codebases. Highly recommended!
+
+                Check out https://www.python.org/ for more info!
+
+                Best,
+                Y
+            """
+# http urls
+pattern = re.compile('http[:/.a-z]+/')
+print re.findall(pattern, my_string)
+
+# TODO: http or https. .com or .org or .net
