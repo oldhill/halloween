f611680ad31f7d8e5ba538a4e5849d6b8cecc840
oldhill <oldhillcode@gmail.com>
2016-01-18 23:54:00 -0800

    C comments, loops, control flow

--- helloworld.c
+++ helloworld.c
@@ -2,6 +2,29 @@
 
 int main()
 {
-  printf ("Hello world!\n");
-}
+  /* hi this is a multi-line comment
+     hi.
+  */
 
+  // single-line comment
+  printf ("Hello world!\n");  // this works too
+
+  // some vars and control flow
+  int numDaysInYear = 365;
+  printf ("days per year: %d\n", numDaysInYear);
+
+  if (numDaysInYear == 366) {
+    printf ("Seems reasonable\n");
+  }
+  else {
+    int long someReallyLongNumber = 100000000000000L;
+    printf ("Some long number: %ld\n", someReallyLongNumber);
+  }
+
+  // loops
+  int i;
+  for (i=0; i<5; i++) {
+    printf ("i=%d\n", i);
+  }
+
+}
