553b949bb67b2f611904d0c3cec1821efdf15e1f
oldhill <oldhillcode@gmail.com>
2016-02-28 22:17:28 -0800

    For and while loops

--- c/drexel.c
+++ c/drexel.c
@@ -13,6 +13,26 @@ float get_pi()
   return M_PI;
 }
 
+// loops
+void the_while()
+{
+  int counter = 0;
+  while (counter < 5)
+  {
+    printf("OK counter is now equal to: %d\n", counter);
+    counter = counter + 1;
+  }
+}
+
+void the_for()
+{
+  for(int i = 0; i < 5; i = i + 1)
+  {
+    printf("OK i is now equal to: %d\n", i);
+  }
+}
+
+// yeah
 int main()
 {
   printf("Hello World!\n");
@@ -23,6 +43,10 @@ int main()
   // user defined fn with return value that's a lib constant
   float my_pi = get_pi();
   printf("OK here's pi: %f\n", my_pi);
+
+  // some loops though
+  the_while();
+  the_for();
 
   return 0;
 }
