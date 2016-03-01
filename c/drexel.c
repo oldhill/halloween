091e90bddd8d2a5e4714833fd71f9f7ebe4bc78f
oldhill <oldhillcode@gmail.com>
2016-02-29 23:07:58 -0800

    String manipulation, two ways of allocating buffers

--- c/drexel.c
+++ c/drexel.c
@@ -1,5 +1,6 @@
 #include <math.h>  // M_PI constant
 #include <stdio.h>
+#include <string.h> // strncopy(), strlen() etc
 
 // definin a random function
 void something()
@@ -36,6 +37,32 @@ void the_for()
 #define CONST_THREE 3
 #define CONST_ZERO 0
 
+// (wildly memory unsafe) conditionals
+char * some_complex_operation(int input_num)
+{
+  if (input_num < 0)
+  {
+    return "That's a negative number";
+  }
+  else if (input_num > 100)
+  {
+    return "That's a pretty big number";
+  }
+  else
+  {
+    return "That's a number between 0 and 100!";
+  }
+}
+
+// maybe safer thing which modifies existing memory buffer
+// passes in a reference to existing buffer... then modifies the buffer...
+void populate_message(char * message, int buffersize)
+{
+  const char * msg = "Hello!";
+  strncpy(message, msg, buffersize - 1); // copy buffersize-1 chars from msg into message
+  message[buffersize - 1] = '\0'; // null terminate
+}
+
 // yeah
 int main()
 {
@@ -58,6 +85,22 @@ int main()
   {
     printf("Counting up to a symbolic constant: %d\n", j);
   }
+
+  // seemingly unsafe non-pre-allocated char pointers
+  char * my_message;
+  my_message = some_complex_operation(5);
+  printf("The message: %s\n", my_message);
+  my_message = some_complex_operation(-1);
+  printf("The message: %s\n", my_message);
+  my_message = some_complex_operation(101);
+  printf("The message: %s\n", my_message);
+
+  // possibly safer pre-allocated memory?
+  char message[15];
+  printf("Initial message: %s\n", message);
+  printf("Initial message length: %ld\n", strlen(message));
+  populate_message(message, sizeof(message));
+  printf("Updated message: %s\n", message);
 
   return 0;
 }
