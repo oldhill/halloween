d3fb4993a134c322e94e0a8a331abe8ceb428cf2
oldhill <oldhillcode@gmail.com>
2016-03-06 18:08:51 -0800

    Fix newlines

--- euler.py
+++ euler.py
@@ -1,9 +1,9 @@
 n = 100000000;
-print 'Finding sum of multiples of 3 and 5 below %d' % n
+print '\nFinding sum of multiples of 3 and 5 below %d' % n
 
 total = 0;
 for i in xrange(0, n):
   if i % 3 == 0 or i % 5 == 0:
     total += i
 
-print 'Result: %d\n' % total
+print 'Result: %d' % total
