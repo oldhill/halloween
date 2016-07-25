23c59a06b001f447802f8a118203baca17ee2c5f
oldhill <oldhillcode@gmail.com>
2016-07-24 22:56:17 -0700

    Use floats instead of ints, print table header

--- c/temperature.c
+++ c/temperature.c
@@ -4,17 +4,21 @@
 
 int main()
 {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0;
     upper = 300;
     step = 20;
 
+    // first print table header
+    printf("%s  %s\n", "Fahr", "Cels");
+
+    // then calculate values and print them to table
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\t\n", fahr, celsius);
+        celsius = (5.0 / 9.0) * (fahr - 32);
+        printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 
